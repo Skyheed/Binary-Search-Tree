9610e8dd6049ead90f8ebe0feb750220859fc441
Skyheed <71256247+Skyheed@users.noreply.github.com>
2021-03-07 23:30:19 +0100

    Add files via upload

---
+++ Binary Tree/Binary Tree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Binary_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = null;
+            Random rng = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                //int x = rng.Next(0, 400);
+                int inputNode = Convert.ToInt32(Console.ReadLine());
+                TreeNode n = new TreeNode(inputNode);
+                if (root == null)
+                {
+                    Console.WriteLine("Setting " + n.value + " to the root");
+                    root = n;
+                }
+                else
+                {
+                    n.NewNode(root);
+                }
+            }
+        }
+    }
+}
---
+++ Binary Tree/Binary Tree/TreeNode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Tree
+{
+    public class TreeNode
+    {
+        public int value;
+        public TreeNode right;
+        public TreeNode left;
+
+        public TreeNode (int data)
+        {
+            value = data;
+            left = null;
+            right = null;
+        }
+
+        public void NewNode(TreeNode root)
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Root var ikke null");
+                return;
+            }
+            else if (root.value == value)
+            {
+                Console.WriteLine("Ingen gentagelser. Tallet er skrevet før");
+                return;
+            }
+            else if (value < root.value)
+            {
+                if (root.left != null)
+                {
+                    NewNode(root.left);
+                }
+                else
+                {
+                    Console.WriteLine("Added " + value + " to the left of " + root.value);
+                    root.left = this;
+                }
+            }
+            else if (value > root.value)
+            {
+                if (root.right != null)
+                {
+                    NewNode(root.right);
+                }
+                else
+                {
+                    Console.WriteLine("Added " + value + " to the right of " + root.value);
+                    root.right = this;
+                }
+            }
+        }
+    }
+}
